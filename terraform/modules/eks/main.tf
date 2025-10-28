f43030d2f70d16100b1fb72b9b48e9e7dac053b3
JunedConnect <Junedwork@hotmail.com>
2025-10-28 14:36:03 +0000

    add coredns with tolerations and also depends on for terraform resroueces

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -35,9 +35,39 @@ resource "aws_eks_cluster" "this" {
 }
 
 resource "aws_eks_addon" "eks-pod-identity-agent" {
-  cluster_name = aws_eks_cluster.this.name
+  cluster_name = var.name
   addon_name = "eks-pod-identity-agent"
   addon_version = "v1.3.9-eksbuild.3"
+  
+  depends_on = [
+    aws_eks_cluster.this,
+    aws_eks_node_group.this
+  ]
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = var.name
+  addon_name                  = "coredns"
+  addon_version               = "v1.11.3-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+
+  configuration_values = jsonencode({
+    nodeSelector = {
+      infra = "true"
+    }
+    tolerations = [
+      {
+        key      = "infra"
+        operator = "Equal"
+        value    = "true"
+      }
+    ]
+  })
+  
+  depends_on = [
+    aws_eks_cluster.this,
+    aws_eks_node_group.this
+  ]
 }
 
 # access entry to allow karpeneter provisioned nodes to access the cluster
@@ -47,7 +77,8 @@ resource "aws_eks_access_entry" "karpenter-node-role" {
   type         = "EC2_LINUX"
 
   depends_on = [
-    aws_eks_cluster.this
+    aws_eks_cluster.this,
+    aws_eks_node_group.this
   ]
 }
 
